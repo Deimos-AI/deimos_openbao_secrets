a9738981169ffc733ef5aafa2c460ced292eb2b9
A0 Agent <tristan@deimosai.com>
2026-03-13 11:58:47 +0000

    refactor: rename plugin to deimos_openbao_secrets
    Rename from openbao_secrets to deimos_openbao_secrets to match
    Deimos plugin naming convention (deimos_memory, deimos_redaction, etc.).
    
    - plugin.yaml: name=deimos_openbao_secrets, title=Deimos OpenBao Secrets
    - All find_plugin_dir() calls updated
    - Gitea repo renamed to deimosAI/deimos_openbao_secrets

--- extensions/python/helpers_secrets_get_secrets_manager_start/_10_openbao_factory.py
+++ extensions/python/helpers_secrets_get_secrets_manager_start/_10_openbao_factory.py
@@ -32,7 +32,7 @@ class OpenBaoFactory(Extension):
         import os
         from python.helpers.plugins import find_plugin_dir
 
-        plugin_dir = find_plugin_dir("openbao_secrets")
+        plugin_dir = find_plugin_dir("deimos_openbao_secrets")
         if not plugin_dir:
             return None
 
